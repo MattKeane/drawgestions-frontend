f95adc293b70c0e30b1d14f14420b39dd986f290
Matt Keane <mattpkeane@gmail.com>
2021-11-13 15:50:56 -0600

    added accessCode to state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
+import { useState } from 'react'
 
 const { REACT_APP_API_URL } = process.env;
 
 function App() {
+  const [accessCode, setAccessCode] = useState('')
+
   const handleStart = async () => {
     try {
       const url = REACT_APP_API_URL + '/room/new'
-      console.log(process.env)
       const startResponse = await fetch(url, {
         method: 'POST',
       })
       const startJson = await startResponse.json()
-      console.log(startJson)
+      setAccessCode(startJson.accessCode)
     } catch (err) {
       console.log(err)
     }
@@ -20,8 +22,16 @@ function App() {
   return (
     <div className="App">
       <h1>Drawgestions</h1>
-      <button onClick={ handleStart }>Start New Game</button>
-      <button>Join Game</button>
+      {
+        accessCode
+        ?
+        <p>You are in room { accessCode }</p>
+        :
+        <>
+          <button onClick={ handleStart }>Start New Game</button>
+          <button>Join Game</button>
+        </>
+      }
     </div>
   );
 }
