fbce9060879cbeb164cae6aad8158a6ab3f3acb5
Matt Keane <mattpkeane@gmail.com>
2021-11-13 16:14:11 -0600

    added ability to join existing room

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 const { REACT_APP_API_URL } = process.env;
 
 function App() {
+  const [room, setRoom] = useState('')
   const [accessCode, setAccessCode] = useState('')
 
   const handleStart = async () => {
@@ -13,7 +14,20 @@ function App() {
         method: 'POST',
       })
       const startJson = await startResponse.json()
-      setAccessCode(startJson.accessCode)
+      setRoom(startJson.accessCode)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleJoin = async () => {
+    try {
+      const url = REACT_APP_API_URL + '/room/join/' + accessCode 
+      const joinResponse = await fetch(url)
+      if (joinResponse.status === 200) {
+        const joinJson = await joinResponse.json()
+        setRoom(joinJson.accessCode)
+      }
     } catch (err) {
       console.log(err)
     }
@@ -23,13 +37,22 @@ function App() {
     <div className="App">
       <h1>Drawgestions</h1>
       {
-        accessCode
+        room
         ?
-        <p>You are in room { accessCode }</p>
+        <p>You are in room { room }</p>
         :
         <>
           <button onClick={ handleStart }>Start New Game</button>
-          <button>Join Game</button>
+          <div>
+            <label htmlFor="accessCode">Room Code:</label>
+            <input 
+              type="text" 
+              id="accessCode"
+              value={ accessCode }
+              onChange={ e => setAccessCode(e.target.value)}
+            />
+            <button onClick={ handleJoin }>Join Game</button>
+          </div>
         </>
       }
     </div>
