d921b4571ef82d05d343aec2a61c6122ea9bb3f4
Matt Keane <mattpkeane@gmail.com>
2021-11-24 16:06:42 -0600

    app now displays assigned suggestion when received

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,8 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Suggestions from './Suggestions'
 
 export default function Play(props) {
     const [drawThis, setDrawThis] = useState('')
+
+    useEffect(() => {
+        props.socket.on('drawThis', suggestion => setDrawThis(suggestion))
+    }, [props.socket])
 
     return (
         <>
